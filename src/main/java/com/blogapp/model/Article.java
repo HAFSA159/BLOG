cdd13540aedeeb14dd5be9d08d96851e111c77a7
WailBoukhari1 <boukahri.wail002@gmail.com>
2024-10-07 14:32:28 +0100

    GES-58 Hibernate and jpa for artical is done

--- src/main/java/com/blogapp/model/Article.java
+++ src/main/java/com/blogapp/model/Article.java
@@ -1,4 +1,48 @@
 package com.blogapp.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Article")
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 10000)
+    private String content;
+
+    @Column(name = "creation_date", nullable = false)
+    private LocalDateTime creationDate;
+
+    @Column(name = "publication_date")
+    private LocalDateTime publicationDate;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id", nullable = false)
+    private ArticleStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments;
+
+    // Getters and setters
 }
